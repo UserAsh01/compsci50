f0dbb724254863fc86f1c957544cb151ededa1db
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-14 12:19:17 +0000

    Tue, Feb 14, 2023, 12:19 PM +00:00

---
+++ einstein/einstein.py
@@ -0,0 +1,4 @@
+mass = int(input("Please enter the mass (in kilograms): "))
+c = 299792458 # speed of light in meters per second
+joules = mass * c**2
+print("The equivalent energy is", joules, "Joules.")46
