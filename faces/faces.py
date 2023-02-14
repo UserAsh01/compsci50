f666ad9d3bc75fb04575958962803d60e4bffb24
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-14 11:29:33 +0000

    Tue, Feb 14, 2023, 11:29 AM +00:00

---
+++ faces/faces.py
@@ -0,0 +1,2 @@
+# Python program that includes a convert function that replaces :) with 🙂 and :( with 🙁,
+# and a main function that prompts the user for input, calls convert on that input, and prints the result
