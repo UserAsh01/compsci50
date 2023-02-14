9c2f410e52d91b1dac529b4df40f9dc425fc8d9b
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-14 11:21:42 +0000

    Tue, Feb 14, 2023, 11:21 AM +00:00

---
+++ playback/playback.py
@@ -0,0 +1,6 @@
+#Python program that promtpts the uer for input and then outputs the same input
+#replacing each space with three periods
+
+input_str = input("Please enter a string: ")
+modified_str = input_str.replaces(" ","...")
+print(modified_str)
