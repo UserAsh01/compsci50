9020e68c6b1f64ab860e634fe658607e63804a6e
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-14 10:30:47 +0000

    Tue, Feb 14, 2023, 10:30 AM +00:00

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,3 +1,9 @@
+#Python program that prompts the user for input and then outputs that same input in lowercase while preserving the punctuation and whitespace
+
+#This program first prompts the user to enter a string by calling the input() function and passing in a prompt string. It then stores the user's input in the input_str variable.
+#The program then calls the lower() method on the input_str variable to convert it to lowercase, and stores the result in the lowercase_str variable.
+#Finally, the program uses the print() function to output the lowercase version of the user's input, while preserving any punctuation and whitespace.
+
 input_str = input("Please enter a string: ")
 lowercase_str = input_str.lower()
 print(lowercase_str)
