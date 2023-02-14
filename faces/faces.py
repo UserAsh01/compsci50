c517e031676781bd9ce6a6c8c04ba98178d318ce
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-14 11:35:49 +0000

    Tue, Feb 14, 2023, 11:35 AM +00:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,4 +1,10 @@
 # Python program that includes a convert function that replaces :) with 🙂 and :( with 🙁,
 # and a main function that prompts the user for input, calls convert on that input, and prints the result
 
-def convert
+def convert(input_str):
+    output_str = input_str.replace(":)", "🙂")
+    output_str = output_str.replace(":(", "🙁")
+    return output_str
+
+def main():
+    input_str = input("please enter a string: ")
