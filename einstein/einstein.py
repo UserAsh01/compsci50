124da0a7254b12cf1358640ac196cae7023f476e
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-14 12:45:15 +0000

    Tue, Feb 14, 2023, 12:45 PM +00:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,5 +1,2 @@
-mass = input("Please enter the mass (in kilograms): ")
-mass = int(mass)
-c = 300000000 # speed of light in meters per second
-joules = mass * c**2
-print(joules)
+c = 300000000 #speed of light in meters per second
+m = int(input())
