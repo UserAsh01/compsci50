b2efbe0c724958fa1c9b563dcc518ed189adef5e
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-14 12:48:17 +0000

    Tue, Feb 14, 2023, 12:48 PM +00:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,2 +1,6 @@
+#Python program that prompts the user for mass in kilograms and then calculates the equivalent energy in Joules
+# using the equation E=mc^2, where c is the speed of light:
 c = 300000000 #speed of light in meters per second
-m = int(input())
+m = int(input("Enter mass in kilograms: "))
+E = m * c**2
+print("Energy in Joules:", E)
