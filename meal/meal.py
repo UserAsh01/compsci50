b8318f34256d0636d7d7b9d974ab8dc839cb58eb
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-22 13:38:07 +0000

    Wed, Feb 22, 2023, 3:38 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,7 +1,14 @@
 def main():
 
+    '''prompts the user for a time and outputs whether it's breakfast, lunch or dinner'''
+    # prompt user for time
+    tme = input()
+
 def convert(time):
     '''converts a time in a 24 hour format to the corresponding number of hours'''
+    hours, minutes = time.split(":")
+    return float(hours) + float(minutes) / 60
+
 
 if __name__ == "__main__":
     main()
