9b226d2ebd5c053668a05010e8d0b04114062528
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-22 13:50:26 +0000

    Wed, Feb 22, 2023, 3:50 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -14,11 +14,11 @@ def main():
 
     #determine meal time based on time
     if 7 <= hours < 8:
-        print("It's breakfast time!")
+        print("breakfast time")
     elif 12 <+hours < 13:
-        print("It's lunch time!")
+        print("lunch time")
     elif 18 <= hours < 19:
-        print("It's dinner time!")
+        print("dinner time")
     else:
         pass #it's not time for a meal
 
