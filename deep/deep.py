9daab862c8f6572e696d6a6a34c5e099cd00c909
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-22 12:01:56 +0000

    Wed, Feb 22, 2023, 2:01 PM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,15 +1,14 @@
 #user input
-user_input = input("What is the answer to the great question of Life, the Universe, and Everything?: ")
+user_input = input("What is the answer to the great question of Life, the Universe, and Everything?: ").lower()
 
+#account for typing variations such as spaces , cases etc
+user_input = user_input.replace(" ","").replace("-","")
 #convert input to string
 answer = str(user_input)
 
 #conditional statement to check if the asnwer is correct or not
-if answer == "42":
+if answer == "42" or answer =="fortytwo" or answer == "forty-two":
    print("Yes")
-elif answer == "forty two":
-   print("Yes")
-elif answer == "forty-two":
-   print("Yes")
+
 else:
    print("no")
