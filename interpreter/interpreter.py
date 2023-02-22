bd79878d82339472833ce03ebca28649ece75d42
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-22 13:05:58 +0000

    Wed, Feb 22, 2023, 3:05 PM +02:00

---
+++ interpreter/interpreter.py
@@ -0,0 +1,18 @@
+#prompt user for math expression
+expr = input("Expression: ")
+
+#split into 3 parts : x, operator, z
+x, op, z = expr.split()
+
+x = int(x)
+z = int(z)
+
+#calulate result based on operator
+if op == "+":
+    result = x + z
+elif op == "-":
+    result = x - z
+elif op == "*":
+    result = x * z
+    
+
