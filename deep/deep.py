f5585fe0af4b72f3b2f2bdf174ac21c3de4b5344
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-22 11:52:06 +0000

    Wed, Feb 22, 2023, 1:52 PM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -7,5 +7,9 @@ answer = str(user_input)
 #conditional statement to check if the asnwer is correct or not
 if answer == "42":
    print("Yes")
+elif answer == "forty two":
+   print("Yes")
+elif answer == "forty-two":
+   print("Yes")
 else:
    print("no")
