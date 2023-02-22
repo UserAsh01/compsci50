85e5780f9e095a58f67c19c2c3c155debd9f45c7
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-22 12:38:21 +0000

    Wed, Feb 22, 2023, 2:38 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -5,4 +5,8 @@ import mimetypes
 filename = input("Please enter the name of a file: ")
 
 #get file suffix and convert to lower case
-suffix = filename.split(".")
+suffix = filename.split(".")[-1].lower() if "." in filename else ""
+
+#determine media type based on suffix
+if suffix in ["gif", "jpg", "jpeg", "png", "pdf", "txt", "zip"]:
+    media_type = mimetypes.type_map["." + suffix]
