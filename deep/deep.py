b293570b7709692e85a5152c3fdead43c6bcd632
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-22 11:48:50 +0000

    Wed, Feb 22, 2023, 1:48 PM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,2 +1,11 @@
 #user input
 user_input = input("What is the answer to the great question of Life, the Universe, and Everything?: ")
+
+#convert input to string
+answer = str(user_input)
+
+#conditional statement to check if the asnwer is correct or not
+if answer == "42":
+   print("Yes")
+else:
+   print("no")
