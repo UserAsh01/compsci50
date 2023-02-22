da6970334ade389d92c9040e431f896ddecd7ca8
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-22 12:30:49 +0000

    Wed, Feb 22, 2023, 2:30 PM +02:00

---
+++ extensions/extensions.py
@@ -0,0 +1,8 @@
+import mimetypes
+# imports the mimetypes module, which provides a database of file types and their corresponding media types.
+
+#promt user for filename
+filename = input("Please enter the name of a file: ")
+
+#get file suffix and convert to lower case
+suffix = filename.spl
