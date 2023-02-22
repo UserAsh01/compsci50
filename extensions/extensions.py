f1359a0041cbe4b356ed6609e5f897bcf734ba8b
UserAsh01 <124042850+UserAsh01@users.noreply.github.com>
2023-02-22 12:49:12 +0000

    Wed, Feb 22, 2023, 2:49 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,7 +2,7 @@ import mimetypes
 # imports the mimetypes module, which provides a database of file types and their corresponding media types.
 
 #promt user for filename
-filename = input("Please enter the name of a file: ")
+filename = input("Please enter the name of a file: ").strip()
 
 #get file suffix and convert to lower case
 suffix = filename.split(".")[-1].lower() if "." in filename else ""
